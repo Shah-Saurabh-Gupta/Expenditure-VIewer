e68e2a738180d1c4f8a9f6e4eacb04913036f73b
Saurabh Gupta <saurabhg@uvxcel.com>
2021-07-25 13:35:32 +0530

    Adding states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,15 @@ import React, { useState } from 'react';
 
 function ExpenseItem(props) {
 
-const [title, setTitle] = useState(props.title);
+    const [title, setTitle] = useState(props.title);
 
-const clickHandler = () => {
-    setTitle('Updated');
-};
+    const clickHandler = () => {
+        setTitle('Updated');
+    };
 
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
