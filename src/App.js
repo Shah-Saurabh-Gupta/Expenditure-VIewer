4e815a774f96eb42ae6ebbc97c3aadbc010999d8
Saurabh Gupta <saurabhg@uvxcel.com>
2021-07-26 21:16:11 +0530

    Add Expense Button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
 
@@ -26,7 +27,7 @@ function App() {
 
   return (
     <div>
-      <h1>hi</h1>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
